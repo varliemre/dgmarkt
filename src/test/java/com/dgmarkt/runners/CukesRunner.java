aefbaa1c75ec7f5579e440c8fd3d5beac42eed1f
Emre VARLI <varliemre02@gmail.com>
2024-06-15 22:02:37 +0200

    Cukes Runner updated

--- src/test/java/com/dgmarkt/runners/CukesRunner.java
+++ src/test/java/com/dgmarkt/runners/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
-                "html:target/default-html-reports.html"
+                "html:target/default-html-reports.html",
+                "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
         glue = "com/dgmarkt/stepDefs",
