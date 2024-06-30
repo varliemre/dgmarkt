3d0fed0278801433a26506fe0dcc557e1ecfb489
Aydin Cakmak <aydin-cakmak45@hotmail.com>
2024-06-30 17:05:18 +0200

    US-11 finished

--- src/test/java/com/dgmarkt/runners/CukesRunner.java
+++ src/test/java/com/dgmarkt/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/dgmarkt/stepDefs",
         dryRun =false,
-        tags =  ""
+        tags =  " "
 )
 public class CukesRunner {
 }
